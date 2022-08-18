018e7e2005b7af375c07ab7f1f6e1b732b747e8c
kevstrosky <kevinochoa.dev@gmail.com>
2022-08-18 13:48:28 -0700

    Test del dropdown agregado y completado

--- cypress/e2e/2-advanced-examples/agregandocarrito.cy.js
+++ cypress/e2e/2-advanced-examples/agregandocarrito.cy.js
@@ -1,27 +1,37 @@
 describe('Agregando un producto al carrito', function(){
     beforeEach(()=>{
-        //Ingresamos a la página
+        //Ingresamos a la página que donde se hará el testing
         cy.visit('https://store.dayglowband.com/');
 
     })
 
-    it('Pulsando botón', function(){
+    it('Entrando a la página del artículo y agregandolo al carrito', function(){
+        //Ingresamos al página del producto
         cy.get(':nth-child(1) > .grid-view-item > .grid-view-item__link').click()
+        //Damos click al botón del producto para agregarlo al carrito
         cy.get('.product-form__item > .btn').click()
 
+        //Con estas tres líneas de código verificamos si existosamente se agregó el producto al carrito
         cy.get('#CartPopupHeading')
         .should('contain.text','Just added to your cart')
         .should('be.visible')
     })
+
 })
 describe('Dropdown', function(){
     beforeEach(()=>{
         //Ingresamos a la página
-        cy.visit('https://erumerch.com/');
+        cy.visit('https://www.w3schools.com/howto/howto_css_dropdown.asp');
 
     })
 
-    it('Verificamos que el drop down de Departamentos, tenga los elementos necesarios', function(){
-        cy.get('.main-navigation > ul > li:nthchild(1) > ul ').invoke('attr', 'style', 'display: block')
+    it('Verificamos que el drop down y sus elementos', function(){
+        cy.get('#main > div.dropdown.dropdown2 > div')        
+        .invoke('attr', 'style', 'display: block')
+        .should('have.attr', 'style', 'display: block')
+        cy.get('.dropdown-content > :nth-child(1)').should('be.visible')
+        cy.get('.dropdown-content > :nth-child(2)').should('be.visible')
+        cy.get('.dropdown-content > :nth-child(3)').should('be.visible')
+
     })
 })
