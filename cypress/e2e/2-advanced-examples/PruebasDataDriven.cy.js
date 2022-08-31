ca678414918ec0a79e388c3ba57d84c45fc9f6ca
kevstrosky <kevinochoa.dev@gmail.com>
2022-08-31 13:46:25 -0700

    archivo de pruebas data driven y objetos para hacer la prueba agregados

---
+++ cypress/e2e/2-advanced-examples/PruebasDataDriven.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="Cypress"/>
+
+//Suite de casos de prueba avanzados
+describe('Segundo conjunto de casos de pruebas avanzadas',function(){
+    before(function(){
+        //Cargamos los valores del archivo example.json en un objeto de datos
+        cy.fixture('example').then(function(datos){
+            this.datos=datos
+        })
+    })
+    beforeEach(()=>{
+        //ingresamos a la pagina de formulario
+        cy.visit('https://demoqa.com/automation-practice-form')
+    })
+
+    it('Llenamos nuestro primer formulario utiizando data',function(){
+        cy.get('#firstName').type(this.datos.nombre)
+        cy.get('#lastName').type(this.datos.apellido)
+        cy.get('#userEmail').type(this.datos.email)
+        cy.get('input[name="gender"][value='+this.datos.sexo+']').check({force:true}).should('be.checked')
+        cy.get('#userNumber').type(this.datos.telefono)
+        cy.get('#dateOfBirthInput').click()
+        cy.get('.react-datepicker__month-select').should('be.visible').select(this.datos.fechaDeNacimiento[0])
+        cy.get('.react-datepicker__year-select').should('be.visible').select(this.datos.fechaDeNacimiento[1])
+        cy.get(':nth-child(1) > .react-datepicker__day--001').click()
+
+
+    })
+})
+Cypress.on('uncaught:exception',(err,runnable)=>{
+    console.log("err :"+ err)
+    console.log("runnable :"+ runnable)
+    return false
+})
